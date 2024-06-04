af4a3551a800e7b696c1831c56db716ec648bc19
BayramBehbudov <bayram.behbudov@gmail.com>
2024-06-04 19:45:15 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+    document.querySelector(".lidPng").classList.add("lidAnim")
+    document.querySelector(".c1Png").classList.add("c1Anim")
+    document.querySelector(".c2Png").classList.add("c2Anim")
+    document.querySelector(".c3Png").classList.add("c3Anim")
+    document.querySelector(".c4Png").classList.add("c4Anim")
+    document.querySelector(".c5Png").classList.add("c5Anim")
+    document.querySelector(".descText").classList.add("descAnim")
+    document.querySelector(".header").classList.add("headerAnim")
+    document.querySelector(".jar").classList.add("jarAnim")
+
+    setTimeout(() => {
+        document.querySelector(".title").classList.add("titleAnim")
+    }, 1500);
+
+}, 1000);
